6741d1e20bb755e80b393b25737f7c3ca4d8ce5b
Marzouk Mariem <36210586+marzoukmariem@users.noreply.github.com>
2019-02-07 21:24:13 +0100

    modification template

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hello5'`, () => {
+  it(`should have as title 'KidsPay'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('hello5');
+    expect(app.title).toEqual('KidsPay');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to hello5!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to KidsPay!');
   });
 });
