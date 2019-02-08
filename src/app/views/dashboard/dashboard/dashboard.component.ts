2e6c9e69a5c3c39995bcd2f10c323ef39fab4f1b
Marzouk Mariem <36210586+marzoukmariem@users.noreply.github.com>
2019-02-08 22:55:56 +0100

    ajout d'une root

--- src/app/views/dashboard/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,7 +8,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 })
 export class DashboardComponent implements OnInit ,OnDestroy{
 
-  constructor() { }
+  constructor( private router: Router) { }
 
   ngOnInit() {
     window.dispatchEvent(new Event('resize'));
@@ -16,5 +17,8 @@ export class DashboardComponent implements OnInit ,OnDestroy{
    ngOnDestroy() :void {
   document.body.className='';
   }
+parent(){
 
+  this.router.navigate(['parent']);
+}
 }
