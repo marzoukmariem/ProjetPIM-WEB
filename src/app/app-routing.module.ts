76664b2d0513525d0689a15b71e443f623939b8e
Anouar-BenOthman <anouar.benothman@esprit.tn>
2019-02-14 15:04:32 +0100

    ajout routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommercantsComponent } from './All-Views/commercants/commercants.component';
+import { CommercantComponent } from './All-Views/commercants/commercant/commercant.component';
+import { StoresComponent } from './All-Views/stores/stores.component';
+import { StoreComponent } from './All-Views/stores/store/store.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'KidsPay/login', pathMatch: 'full'}
+  {path: '', redirectTo: 'KidsPay/login', pathMatch: 'full'},
+  {path: 'KidsPay/AceuilAdmin/commercants', component: CommercantsComponent},
+  {path: 'KidsPay/AceuilAdmin/commercant',children:[
+    {path:'',component: CommercantComponent},
+    {path:'edit/:id', component:CommercantComponent}
+  ]},
+  {path: 'KidsPay/AceuilAdmin/stores', component: StoresComponent},
+  {path: 'KidsPay/AceuilAdmin/store',children:[
+    {path:'',component: StoreComponent},
+    {path:'edit/:id', component:StoreComponent}
+  ]}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './views/login/login/login.component';
-import {DashboardModule} from "./views/dashboard/dashboard.module";
-import {LoginRoutingModule} from "./views/login--rounting/login--rounting.module";
-import {ActivatedRoute, RouterModule} from "@angular/router";
-import {DashboardComponent} from "./views/dashboard/dashboard/dashboard.component";
+import { DashboardModule } from "./views/dashboard/dashboard.module";
+import { LoginRoutingModule } from "./views/login--rounting/login--rounting.module";
+import { ActivatedRoute, RouterModule } from "@angular/router";
+import { DashboardComponent } from "./views/dashboard/dashboard/dashboard.component";
 import { ParentsListComponent } from './All-Views/parents/parents-list/parents-list.component';
-import {ParentsComponent} from "./All-Views/parents/parents.component";
-import {ParentComponent} from "./All-Views/parents/parent/parent.component";
-import {ParentService} from "./Services/parent.service";
-import {FormsModule} from "@angular/forms";
+import { ParentsComponent } from "./All-Views/parents/parents.component";
+import { ParentComponent } from "./All-Views/parents/parent/parent.component";
+import { ParentService } from "./Services/parent.service";
+import { FormsModule } from "@angular/forms";
 import { EnfantComponent } from './All-Views/enfant/enfant.component';
 import { UserComponent } from './All-Views/user/user.component';
 import { AcceuilParentComponent } from './views/acceuil-parent/acceuil-parent.component';
 import { CommAcceuilComponent } from './views/comm-acceuil/comm-acceuil.component';
 import { CommercantsComponent } from './All-Views/commercants/commercants.component';
 import { CommercantComponent } from './All-Views/commercants/commercant/commercant.component';
-import { CommercantItemsComponent } from './All-Views/commercants/commercant-items/commercant-items.component';
-import {LayoutModule} from "./views/layout/layout.module";
-import { StoreComponent } from './All-Views/store/store.component';
+import { LayoutModule } from "./views/layout/layout.module";
+import { StoreComponent } from './All-Views/stores/store/store.component';
 import { StoresComponent } from './All-Views/stores/stores.component';
 import { HistoriqueComponent } from './All-Views/historique/historique.component';
 
@@ -43,33 +42,28 @@ import { HistoriqueComponent } from './All-Views/historique/historique.component
     CommAcceuilComponent,
     CommercantsComponent,
     CommercantComponent,
-    CommercantItemsComponent,
     StoreComponent,
     StoresComponent,
     HistoriqueComponent,
 
-      ],
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-     DashboardModule,
+    DashboardModule,
     LoginRoutingModule,
     HttpClientModule,
     FormsModule,
     LayoutModule,
 
-       RouterModule.forRoot([
-         {path: 'KidsPay/Aceuilparent/Historique', component: HistoriqueComponent},
-         {path: 'KidsPay/AceuilAdmin/commercant',children:[
-             {path:'',component: CommercantComponent},
-             {path:'edit/:id', component:CommercantComponent}
-           ]},
-         {path: 'KidsPay/AceuilAdmin/commercants', component: CommercantsComponent},
-       {path: 'KidsPay/AceuilComerçant', component: CommAcceuilComponent},
-      {path: 'KidsPay/Aceuilparent', component: AcceuilParentComponent},
-      {path: 'KidsPay/AceuilAdmin/parents', component: ParentsComponent},
-         {path: 'KidsPay/AceuilAdmin/parents/enfants', component: EnfantComponent},
-      {path: 'KidsPay/AceuilAdmin', component: DashboardComponent}
+    RouterModule.forRoot([
+      { path: 'KidsPay/Aceuilparent/Historique', component: HistoriqueComponent },
+      { path: 'KidsPay/AceuilAdmin/commercants', component: CommercantsComponent },
+      { path: 'KidsPay/AceuilComerçant', component: CommAcceuilComponent },
+      { path: 'KidsPay/Aceuilparent', component: AcceuilParentComponent },
+      { path: 'KidsPay/AceuilAdmin/parents', component: ParentsComponent },
+      { path: 'KidsPay/AceuilAdmin/parents/enfants', component: EnfantComponent },
+      { path: 'KidsPay/AceuilAdmin', component: DashboardComponent }
     ])
 
   ],
