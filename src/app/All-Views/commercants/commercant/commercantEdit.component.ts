192d9cc6d9222d8273b51d2e51b3a96bb1f690b7
Anouar-BenOthman <anouar.benothman@esprit.tn>
2019-05-04 00:57:53 +0100

    Control saisie Commercant

--- src/app/All-Views/commercants/commercant/commercantEdit.component.ts
+++ src/app/All-Views/commercants/commercant/commercantEdit.component.ts
@@ -10,7 +10,10 @@ declare var $: any;
   styleUrls: ["./commercant.component.css"]
 })
 export class CommercantEditComponent implements OnInit {
-
+  _code_: number;
+  _codeNum_: number;
+  _codeNom_: number;
+  _codeCin_: number;
   constructor(private service: CommercantService,
     private router: Router,
     private currentRoute:ActivatedRoute) { }
@@ -46,13 +49,119 @@ export class CommercantEditComponent implements OnInit {
   }
 
   onSubmit(form:NgForm){
+
+    this.hasDigitNomFind(this.service.formData.nom)
+    this.hasDigitFind(this.service.formData.prenom)
+    this.hasNoDigitFind(this.service.formData.numTel)
+    this.hasNoDigitCinFind(this.service.formData.cin)
+    console.log(this._code_,'_code_ prenom')
+    console.log(this._codeNom_,'_codeNom_ nom')
+    console.log(this._codeNum_,'codeNum')
+    console.log(this._codeCin_,'codeCin')
+    if (this._code_ == -1 || this._code_ == 10 || this._codeNom_ == -1 || this._codeNom_ == 10|| !this.isEmail(this.service.formData.email ) || this._codeNum_ == 1 || this._codeNum_ == 10|| this._codeCin_ == 1 || this._codeCin_ == 10 || this.service.formData.numTel.length<8 ||  this.service.formData.cin.length!=8 || String(this.service.formData.password).length==0 ) {
+      alert('veuillez valider tous les champs')
+    } else {
+
     $('#spinner2').show();
     this.service.UpdateCommercant().subscribe(res =>{
       this.resetForm();
-      alert('Submittedsuccessfully')
+      alert('mise a jour effectuée')
       this.router.navigate(['KidsPay/AceuilAdmin/commercants'])
     })
   }
+
+}
+
+
+
+hasDigitNomFind(_str_) {
+  this._codeNom_ = 10;  /*When empty string found*/
+  var _strArray = [];
+
+  if (_str_ !== '' || _str_ !== undefined || _str_ !== null) {
+    _strArray = _str_.split('');
+    for (var i = 0; i < _strArray.length; i++) {
+      if (!isNaN(parseInt(_strArray[i]))) {
+        this._codeNom_ = -1;
+        break;
+      } else {
+        this._codeNom_ = 1;
+      }
+    }
+
+  }
+  return this._codeNom_;
+}
+
+hasDigitFind(_str_) {
+  this._code_ = 10;  /*When empty string found*/
+  var _strArray = [];
+
+  if (_str_ !== '' || _str_ !== undefined || _str_ !== null) {
+    _strArray = _str_.split('');
+    for (var i = 0; i < _strArray.length; i++) {
+      if (!isNaN(parseInt(_strArray[i]))) {
+        this._code_ = -1;
+      } else {
+        this._code_ = 1;
+      }
+    }
+
+  }
+  return this._code_;
+}
+
+hasNoDigitFind(_str_) {
+  this._codeNum_ = 10;  /*When empty string found*/
+  var _strArray = [];
+
+  if (_str_ !== '' || _str_ !== undefined || _str_ !== null) {
+    _strArray = _str_.split('');
+    for (var i = 0; i < _strArray.length; i++) {
+      if (!isNaN(parseInt(_strArray[i]))) {
+        this._codeNum_ = -1;
+      } else {
+        this._codeNum_ = 1;
+      }
+    }
+
+  }
+  return this._codeNum_;
+}
+
+hasNoDigitCinFind(_str_) {
+  this._codeCin_ = 10;  /*When empty string found*/
+  var _strArray = [];
+
+  if (_str_ !== '' || _str_ !== undefined || _str_ !== null) {
+    _strArray = _str_.split('');
+    for (var i = 0; i < _strArray.length; i++) {
+      if (!isNaN(parseInt(_strArray[i]))) {
+        this._codeCin_ = -1;
+        break;
+      } else {
+        this._codeCin_ = 1;
+      }
+    }
+
+  }
+  return this._codeCin_;
+}
+
+
+isEmail(search:string):boolean
+{
+    var  serchfind:boolean;
+
+    var regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
+
+    serchfind = regexp.test(search);
+
+    console.log(serchfind)
+    return serchfind
+}
+
 
 }
 
